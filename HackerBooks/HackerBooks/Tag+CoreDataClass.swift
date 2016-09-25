479aa6e1bc7d291c8addaba82be628f3f656c316
amdaza <amdazacastillo@gmail.com>
2016-09-25 18:13:19 +0200

    Add equatable and comparable to Tag class

--- HackerBooks/HackerBooks/Tag+CoreDataClass.swift
+++ HackerBooks/HackerBooks/Tag+CoreDataClass.swift
@@ -12,4 +12,35 @@ import CoreData
 
 public class Tag: NSManagedObject {
 
+    //MARK: - Proxies
+    var proxyForComparison : String{
+        get {
+            return name!
+        }
+    }
+    
+    var proxyForSorting: String {
+        get {
+            let fav = (name == "favourites") ? "A" : "Z"
+            
+            return "\(fav)\(name)"
+        }
+    }
 }
+
+// MARK: - Equatable
+public func ==(lhs: Tag, rhs: Tag) -> Bool {
+    
+    guard (lhs !== rhs) else {
+        return true
+    }
+    return lhs.proxyForComparison == rhs.proxyForComparison
+    
+}
+
+// MARK: - Comparable
+public func <(lhs: Tag, rhs: Tag) -> Bool {
+    
+    return lhs.proxyForSorting < rhs.proxyForSorting
+}
+
