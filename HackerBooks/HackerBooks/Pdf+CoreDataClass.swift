99426329eed3a5c515cd07f8eeae2917b30f295e
amdaza <amdazacastillo@gmail.com>
2016-10-02 14:16:22 +0200

    Convenience inits for Pdf class

--- HackerBooks/HackerBooks/Pdf+CoreDataClass.swift
+++ HackerBooks/HackerBooks/Pdf+CoreDataClass.swift
@@ -14,4 +14,33 @@ public class Pdf: NSManagedObject {
     
     static let entityName = "Pdf"
 
+    convenience init(book: Book, remoteUrl: String,
+                     inContext context: NSManagedObjectContext) {
+        // Get entity description
+        let ent = NSEntityDescription.entity(forEntityName: Pdf.entityName,
+                                             in: context)!
+        
+        // Call super
+        self.init(entity: ent, insertInto: context)
+        
+        self.book = book
+        self.remoteUrl = remoteUrl
+    }
+    
+    convenience init(book: Book,
+                     pdfData: NSData,
+                     inContext context: NSManagedObjectContext) {
+        
+        let ent = NSEntityDescription.entity(forEntityName: Pdf.entityName,
+                                             in: context)!
+        
+        self.init(entity: ent, insertInto: context)
+        
+        // Add note
+        self.book = book
+        
+        // Transform UIImage into data and set it
+        self.pdfData = pdfData
+        
+    }
 }
