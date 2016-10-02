96d8d5ec406e2c57d387c395a3dd3a2cb7bac95b
amdaza <amdazacastillo@gmail.com>
2016-10-02 18:49:36 +0200

    Upsert for pdf, Also added remoteUrl in init

--- HackerBooks/HackerBooks/Pdf+CoreDataClass.swift
+++ HackerBooks/HackerBooks/Pdf+CoreDataClass.swift
@@ -14,7 +14,8 @@ public class Pdf: NSManagedObject {
     
     static let entityName = "Pdf"
 
-    convenience init(book: Book, remoteUrl: String,
+    convenience init(book: Book,
+                     remoteUrl: String,
                      inContext context: NSManagedObjectContext) {
         // Get entity description
         let ent = NSEntityDescription.entity(forEntityName: Pdf.entityName,
@@ -29,6 +30,7 @@ public class Pdf: NSManagedObject {
     }
     
     convenience init(book: Book,
+                     remoteUrl: String,
                      pdfData: NSData,
                      inContext context: NSManagedObjectContext) {
         
@@ -37,14 +39,55 @@ public class Pdf: NSManagedObject {
         
         self.init(entity: ent, insertInto: context)
         
-        // Add note
+        // Add Pdf
         self.book = book
-        
-        // Transform UIImage into data and set it
+        self.remoteUrl = remoteUrl
         self.pdfData = pdfData
         self.loaded = false
         
     }
+    
+    // MARK: Static get & update functions
+    
+    // Get Pdf if exists, nil if it doesn't
+    public static func getIfExists(remoteUrl: String,
+                                   inContext context: NSManagedObjectContext) -> Pdf? {
+        
+        // Check if Tag already exists
+        let req = NSFetchRequest<Pdf>(entityName: Pdf.entityName)
+        req.predicate = NSPredicate(format: "remoteUrl == %@", remoteUrl)
+        
+        if let result = try? context.fetch(req),
+            result.count > 0 {
+                
+            return result.first!
+        } else {
+            return nil
+        }
+    }
+    
+    // Upsert -> update or insert
+    // Update if exist, insert if it doesn't
+    // Return created or updated Pdf
+    public static func upsert(book: Book,
+                              remoteUrl: String,
+                              inContext context: NSManagedObjectContext) -> Pdf {
+        
+        if let pdf = Pdf.getIfExists(remoteUrl: remoteUrl,
+                                     inContext: context) {
+            // Get Pdf
+            return pdf
+            
+        } else {
+            // Create Pdf
+            return Pdf(book: book,
+                       remoteUrl: remoteUrl,
+                       inContext: context)
+        }
+    }
+
+    
+    // MARK: - Get Async Data
     
     func downloadPdf() {
         
