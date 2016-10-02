b8241fb67533b95a69fdc9d7981319b7e0ab3f2d
amdaza <amdazacastillo@gmail.com>
2016-10-02 14:48:32 +0200

    Download, get pdf and loaded

--- HackerBooks/HackerBooks/Pdf+CoreDataClass.swift
+++ HackerBooks/HackerBooks/Pdf+CoreDataClass.swift
@@ -25,6 +25,7 @@ public class Pdf: NSManagedObject {
         
         self.book = book
         self.remoteUrl = remoteUrl
+        self.loaded = false
     }
     
     convenience init(book: Book,
@@ -41,6 +42,44 @@ public class Pdf: NSManagedObject {
         
         // Transform UIImage into data and set it
         self.pdfData = pdfData
+        self.loaded = false
         
+    }
+    
+    func downloadPdf() {
+        
+        let queue = DispatchQueue(label: "downloadData", attributes: [])
+        
+        queue.async {
+            if let url = URL(string: self.remoteUrl!),
+                let data = try? Data(contentsOf: url) {
+                
+                DispatchQueue.main.async {
+                    self.pdfData = NSData(data: data)
+                    self.loaded = true
+                    /*
+                     // Notify
+                     let nc = NotificationCenter.default
+                     let notif = Notification(name: Notification.Name(rawValue: ImageDidChangeNotification), object: self,
+                     userInfo: [ImageKey: self.remoteUrl.path])
+                     
+                     nc.post(notif)
+                     */
+                }
+            } else {
+                //throw HackerBooksError.resourcePointedByUrLNotReachable
+                
+            }
+        }
+    }
+    
+    func getPdf() {
+        if (!loaded) {
+            
+            // File doesn't exists. Download
+            downloadPdf()
+            
+            
+        }
     }
 }
